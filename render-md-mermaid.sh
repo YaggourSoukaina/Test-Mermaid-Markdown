d06e9a7f1b2b08eb8ebe6266d3e28a57b4df3865
YaggourSoukaina <90036175+YaggourSoukaina@users.noreply.github.com>
2021-11-30 17:34:55 +0100

    Update render-md-mermaid.sh

--- render-md-mermaid.sh
+++ render-md-mermaid.sh
@@ -15,3 +15,57 @@
 #    C -->|Three| F[fa:fa-car Car]
 # ```
 # </details>
+#
+# The script will pick up the graph definition from the mermaid code block and render it to the image file and path specified in the
+# image tag using mermaid-cli. The rendered image can be in svg or png format, whatever is specified will be generated.
+
+
+set -eu
+if [ "$1" == "" ]; then
+  echo "$(tput setaf 1)No Markdown document specified$(tput sgr0)"
+  echo ""
+  cat $0 | grep -E "^#" | grep -Ev "^#!/" | sed -E 's/^#[ ]?//'
+  exit 1
+fi
+markdown_input=$1
+image_re=".*\.(svg|png)$"
+echo "Markdown file: $markdown_input"
+
+rm -f .render-md-mermaid-config.json .render-md-mermaid.css
+mermaid_config='{"flowchart": {"useMaxWidth": false }}'
+mermaid_css='#container > svg { max-width: 100% !important; }'
+echo "$mermaid_config" >> .render-md-mermaid-config.json
+echo "$mermaid_css" >> .render-md-mermaid.css
+
+mermaid_file=""
+IFS=$'\n'
+for line in $(perl -0777 -ne 'while(m/!\[.*?\]\(([^\)]+)\)\n+<details>([\s\S]*?)```mermaid\n([\s\S]*?)\n```/g){print "$1\n$3\n";} ' "$markdown_input")
+do
+    if [[ $line =~ $image_re ]]; then
+        mermaid_file="$line.mermaid"
+        if [[ ! "$mermaid_file" =~ ^.*/.* ]]; then
+            mermaid_file="./$mermaid_file"
+        fi
+        mkdir -p -- "${mermaid_file%/*}"
+    else
+        if [[ ! "$mermaid_file" = "" ]]; then
+            echo "$line" >> "$mermaid_file"
+        fi
+    fi
+done;
+for mermaid_img in $(find . -name "*.mermaid" | sed -E 's/((.*).mermaid)/\2|\1/')
+do
+    image_file=${mermaid_img%|*}
+    mermaid_file=${mermaid_img#*|}
+    if [[ "$2" == "in-container" ]]; then
+        /home/mermaidcli/node_modules/.bin/mmdc -p /puppeteer-config.json -o "$image_file" -i "$mermaid_file" -t neutral -C ".render-md-mermaid.css" -c ".render-md-mermaid-config.json" -s 4
+    else
+        docker run --rm -t -v "$PWD:/data" minlag/mermaid-cli:latest -o "/data/$image_file" -i "/data/$mermaid_file" -t neutral -C "/data/.render-md-mermaid.css" -c "/data/.render-md-mermaid-config.json" -s 4
+    fi
+    if [[ "$image_file" =~ ^.*\.svg$ ]]; then
+        sed -i.bak -e 's/<br>/<br\/>/g' $image_file
+    fi
+    echo "Generated: $image_file"
+    rm -f "$mermaid_file" "$image_file.bak"
+done
+rm -f .render-md-mermaid-config.json .render-md-mermaid.css
