7e868573a36b6b64d9788ebba4d7fe86f695cad9
YaggourSoukaina <90036175+YaggourSoukaina@users.noreply.github.com>
2021-12-09 11:40:36 +0100

    Update render-md-mermaid.mk

--- render-md-mermaid.mk
+++ render-md-mermaid.mk
@@ -2,3 +2,6 @@
 
 render-md-mermaid: $(shell find . -name "render-md-mermaid.sh") $(shell find . -name "*.md") ## Render all mermaid graphs in any .md file in the repository
 	@for md in $(shell find . -name "*.md"); do "./$<" "$$md"; done
+	
+demo-md-mermaid: $(shell find . -name "demo-md-mermaid.sh") $(shell find . -name "*.md") ## Render all mermaid graphs in any .md file in the repository
+	@for md in $(shell find . -name "*.md"); do "./$<" "$$md"; done
