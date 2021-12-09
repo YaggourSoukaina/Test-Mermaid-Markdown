6174b3f78a831e5788fc09dab1405550f283db33
YaggourSoukaina <90036175+YaggourSoukaina@users.noreply.github.com>
2021-12-09 11:28:10 +0100

    Create demo-md-mermaid.sh

---
+++ demo-md-mermaid.sh
@@ -0,0 +1,15 @@
+![rendered image description](relative/path/to/demo_rendered_image. svg or png )
+<details>
+  <summary>diagram source</summary>
+  This details block is collapsed by default when viewed in GitHub. This hides the mermaid graph definition, while the rendered image
+  linked above is shown. The details tag has to follow the image tag. (newlines allowed)
+
+```mermaid
+graph LR 
+    A[Christmas] -->|Get money| B(Go shopping) 
+    B --> C{Let me think} 
+    C -->|One| D[Laptop] 
+    C -->|Two| E[iPhone] 
+    C -->|Three| F[fa:fa-car Car]
+```
+</details>
