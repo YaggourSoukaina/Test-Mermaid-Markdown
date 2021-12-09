be94439ef696fd71fa0adfaec01663be4260ac72
YaggourSoukaina <90036175+YaggourSoukaina@users.noreply.github.com>
2021-12-09 11:33:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 for md in $(find . -name "*.md"); do /render-md-mermaid.sh "$md" in-container; done
+for md in $(find . -name "*.md"); do /demo-md-mermaid.sh "$md" in-container; done
