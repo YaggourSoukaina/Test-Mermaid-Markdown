1e03dafd0b611093fc527585ec1d7a7e474a7dd9
YaggourSoukaina <90036175+YaggourSoukaina@users.noreply.github.com>
2021-12-21 15:32:26 +0100

    Update render-md-mermaid.sh

--- render-md-mermaid.sh
+++ render-md-mermaid.sh
@@ -3,18 +3,19 @@
 #
 # This can be invoked on any Markdown file to render embedded mermaid diagrams, provided they are presented in the following format:
 #
-# ![rendered image description](output/rendered_image.png)
+# ![rendered image description](relative/path/to/demo_rendered_image.png.png)
 # <details>
 #   <summary>diagram source</summary>
 #   This details block is collapsed by default when viewed in GitHub. This hides the mermaid graph definition, while the rendered image
 #   linked above is shown. The details tag has to follow the image tag. (newlines allowed)
 #
 # ```mermaid
-# graph TD;
-#    A-->B;
-#    A-->C;
-#    B-->D;
-#    C-->D;
+# Graph LR 
+#    A[Christmas] -->|Get money| B(Go shopping) 
+#    B --> C{Let me think} 
+#    C -->|One| D[Laptop] 
+#    C -->|Two| E[iPhone] 
+#    C -->|Three| F[fa:fa-car Car]
 # ```
 # </details>
 #
